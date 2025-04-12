4d331ee3c7d03db13a855ef0424ed7d73b6736b1
David Irvin <davidirvin47@gmail.com>
2025-04-12 08:14:48 -0600

    Properly type error

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { initApp } from './__core/init'
 import { Logger } from './__core/logger'
 
-initApp().catch((error) => {
+initApp().catch((error: unknown) => {
   Logger.error('Error initializing app:', error)
   process.exit(1)
 })
